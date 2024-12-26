80ff3b559d06fe1108e9459e8ad1aa675772b0a3
Paras jain <120381899+1234Paras@users.noreply.github.com>
2024-12-26 21:53:34 +0530

    Add files via upload

---
+++ LargestthreenumberIs.java
@@ -0,0 +1,21 @@
+public class LargestthreenumberIs {
+    public static void main(String args[]){
+
+        int a = 1, b = 4, c = 9;
+
+        if((a>=b) && (a>=c)){
+            System.out.println("A is the greatest number");
+        }
+        else if((b>=a) && (b>=c)){
+            System.out.println("B is the greatest number");
+        }
+        else{
+            System.out.println("C is the greatest number");
+        }
+
+        int number = 5;
+
+        String type = ((number%2)==0) ? "even" : "odds";
+        System.out.println(type);
+    }
+}
