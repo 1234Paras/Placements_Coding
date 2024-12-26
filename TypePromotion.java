3429c8ebe5a082ee07203bdec9defc01d0739b7d
Paras jain <120381899+1234Paras@users.noreply.github.com>
2024-12-26 21:53:52 +0530

    Add files via upload

---
+++ TypePromotion.java
@@ -0,0 +1,30 @@
+public class TypePromotion {
+    public static void main(String args[]){
+        /*char a = 'a';
+        char b = 'b';
+
+        System.out.println((int) (a));
+        System.out.println((int)(b));
+        System.out.println(b-a);*/
+
+        //Type Promotion
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
+        System.out.println(bt);
+
+        //Type Promotion2
+        int a1 = 10;
+        float b1 = 20.25f;
+        long c1 = 25;
+        double d1 = 40;
+        double answ = a1 + b1 + c1 + d1;
+        System.out.println(answ);
+
+        //Type Casting
+        byte a2 = 5;
+        a2 = (byte) (a2 * 2);
+        System.out.println(a2);
+    }
+}
