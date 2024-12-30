a724fa6e618ee375af36ac88daf876fa4e6f7b44
Paras jain <120381899+1234Paras@users.noreply.github.com>
2024-12-30 13:49:09 +0530

    Add files via upload

---
+++ HollowRectanglePattern6.java
@@ -0,0 +1,20 @@
+public class HollowRectanglePattern6 {
+
+    public static void HollowRectangle(int totRows, int totCols){
+
+        for(int i=1; i<=totRows; i++){
+            for(int j=1; j<=totCols; j++){
+                if(i == 1 || i == totRows || j == 1 || j == totCols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        HollowRectangle(4,4);
+    }
+}
