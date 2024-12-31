f8183a1b3a8bb781a817b48101a222374b02659d
Paras jain <120381899+1234Paras@users.noreply.github.com>
2024-12-31 15:38:42 +0530

    Add files via upload

---
+++ Patterns Program in java/PalindromicnumberPattern.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PalindromicnumberPattern {
+    
+    public static void PalindromicPatt(int n){
+        for(int i=1; i<=n; i++){
+
+            //Spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //Descending
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+
+            //Ascending
+            for(int j=2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int n = sc.nextInt();
+        PalindromicPatt(n);
+    }
+}
