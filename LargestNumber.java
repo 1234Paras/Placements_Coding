0da0f66244ff71e6a922c0f3ae50d87b5fba3e15
Paras jain <120381899+1234Paras@users.noreply.github.com>
2025-01-02 21:40:18 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,22 @@
+public class LargestNumber {
+
+    public static int LargestNo(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest Value : " + smallest);
+        return largest;
+    }
+    public static void main(String args[]){
+        int numbers[] = {1,2,6,7,5};
+        System.out.println("Largest Value : " + LargestNo(numbers));
+    }
+}
