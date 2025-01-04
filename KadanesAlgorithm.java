d1288c3de27707c8817261e3276e40aab430e651
Paras jain <120381899+1234Paras@users.noreply.github.com>
2025-01-04 09:35:03 +0530

    KadanesAlgorithm.java

--- KadanesAlgorithm.java
+++ KadanesAlgorithm.java
@@ -19,3 +19,25 @@ public class KadanesAlgorithm {
         KadanesAlgo(numbers);
     }
 }
+
+//Leetcode solutions
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<nums.length; i++){
+            cs = cs + nums[i];
+
+            if(cs > ms){
+                ms = cs;
+            }
+
+            if(cs<0){
+                cs = 0;
+            }
+            // ms = Math.max(cs,ms);
+        }
+        return ms;
+    }
+}
