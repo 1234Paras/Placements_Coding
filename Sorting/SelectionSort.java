cfc1197a947c69c86237a27c6a01020ac2db1c7e
Paras jain <120381899+1234Paras@users.noreply.github.com>
2025-01-06 13:33:02 +0530

    Add files via upload

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class SelectionSort {
+
+    public static void Selectionsorting(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int minIndex = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minIndex] > arr[j]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {2,1,7,5,9};
+        Selectionsorting(arr);
+        printArray(arr);
+    }    
+}
