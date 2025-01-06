c0d554cc00729b922e4acd2fc755ac561aa6f58e
Paras jain <120381899+1234Paras@users.noreply.github.com>
2025-01-06 13:38:02 +0530

    BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -26,3 +26,21 @@ public class BubbleSort{
         printArray(arr);
     }
 }
+
+//Optimized Funtion of Bubble Sorting, If array is the Sorted Manner
+/*public static void Bubblesortingoptimizedcode(int arr[]){
+    for(int i=0; i<arr.length-1; i++){
+        boolean isSwap = false;
+        for(int j=0; j<arr.length-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                isSwap = true;
+            }
+        }
+    if(!isSwap){
+        return;
+    }
+    }
+}*/
