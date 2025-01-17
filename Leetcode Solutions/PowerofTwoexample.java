21194e1952b0ec59d0c979481724fef6e8842e89
Paras jain <120381899+1234Paras@users.noreply.github.com>
2025-01-17 08:08:50 +0530

    PowerofTwoexample.java

--- Leetcode Solutions/PowerofTwoexample.java
+++ Leetcode Solutions/PowerofTwoexample.java
@@ -2,12 +2,16 @@ public class PowerofTwoexample {
 
     public static boolean isPowerofTwo(int n){
 
+        int ans = 1;
         for(int i=0; i<=30; i++){
 
-            int ans = (int) Math.pow(2,i);
+            // int ans = (int) Math.pow(2,i);
 
             if(ans == n){
                 return true;
+            }
+            if(ans < Integer.MAX_VALUE/2){
+                ans = ans * 2;
             }
         }
         return false;
